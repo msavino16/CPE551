847f1e358c9725ff79d6e3180fa02a020473e411
msavino16 <michaelsavino16@gmail.com>
2024-09-03 19:44:52 -0400

    LAB01 upload

---
+++ Lab01/hello2.py
@@ -0,0 +1,6 @@
+# Author: Michael Savino
+# Date: 9/4/2024
+# Description: Simple Hello World with Name
+
+userName = input("Please enter your name: ")
+print(f"Hello, {userName}!")
---
+++ Lab01/hello3.py
@@ -0,0 +1,7 @@
+# Author: Michael Savino
+# Date: 9/4/2024
+# Description: Simple Hello World with Age and Name
+
+userName = input("Please enter your name: ")
+userAge = input("Please enter your age: ")
+print(f"Hello, {userName} you are {userAge} years old!")
